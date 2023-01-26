a46828f3fc47e4b00fe5ebb0ef1dbe988711cdbd
RitikKumar202 <ritik2002july@gmail.com>
2023-01-26 14:24:25 +0530

    created notes making app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CreateNote from "./pages/CreateNote";
+import EditNote from "./pages/EditNote";
+import Notes from "./pages/Notes";
+
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || [])
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <main id="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Notes notes={notes} />} />
+          <Route path="/create-note" element={<CreateNote setNotes={setNotes} />} />
+          <Route path="/edit-note/:id" element={<EditNote notes={notes} setNotes={setNotes} />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
---
+++ src/pages/CreateNote.js
@@ -0,0 +1,41 @@
+import { useState } from "react"
+import { IoIosArrowBack } from "react-icons/io"
+import { Link, useNavigate } from 'react-router-dom'
+import { v4 as uuid } from "uuid"
+
+import useCreateDate from "../components/useCreateDate"
+
+export default function CreateNote({setNotes}) {
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const date = useCreateDate();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (title && description) {
+            const note = { id: uuid(), title, description, date }
+            // add this note to the note array
+            setNotes(prevNotes => [note, ...prevNotes])
+
+            // redirect home page
+            navigate('/');
+        }
+    }
+
+    return (
+        <section>
+            <header className="create-note__header">
+                <Link to="/" className="btn" ><IoIosArrowBack /></Link>
+                <button className='btn lg primary' onClick={handleSubmit}>Add Notes</button>
+            </header>
+
+            <form className="create-note__form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Title" autoFocus value={title} onChange={(e) => setTitle(e.target.value)} />
+                <textarea rows="28" placeholder="Note description..." value={description} onChange={(e) => setDescription(e.target.value)} ></textarea>
+            </form>
+        </section>
+    )
+}
