f395e1364467c87ec6430da929d28303494fd0d2
m-eraf <eraf9120@gmail.com>
2023-01-28 12:39:30 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CreateNote from "./pages/CreateNote";
-import EditNote from "./pages/EditNote";
-import Notes from "./pages/Notes";
 
-import { useEffect, useState } from "react";
+//components
+import Home from './components/Home';
+import DataProvider from './context/DataProvider';
 
 function App() {
-
-  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem('notes')) || [])
-
-  useEffect(() => {
-    localStorage.setItem('notes', JSON.stringify(notes))
-  }, [notes])
-
   return (
-
-    <main id="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Notes notes={notes} />} />
-          <Route path="/create-note" element={<CreateNote setNotes={setNotes} />} />
-          <Route path="/edit-note/:id" element={<EditNote notes={notes} setNotes={setNotes} />} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+    <DataProvider>
+      <Home />
+    </DataProvider>
   );
 }
 
